d02736852e11ddb5f8a0134c719b7467b6ffd307
JAUME PONS ORTÍ <jpons@iesfuentesanluis.org>
2020-10-05 12:05:17 +0200

    Añadido ahorcado 1

---
+++ Ahorcado 1/Propuesta inicial/js/index.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    alert("onloadd")
+  document.getElementById("empezar").addEventListener("click","mouetraroculta")
+};
+
+function mouetraroculta(){
+    alert("mostrando")
+}
