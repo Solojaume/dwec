8ebf7ad71c9b44288cff786e8e5426d2416e76b1
JAUME PONS ORTÍ <jpons@iesfuentesanluis.org>
2020-10-05 20:41:48 +0200

    Ahorcado 2 separado en Modelo Vista Controlador

---
+++ Ahorcado 2/js/modelo/ahorcado.js
@@ -0,0 +1,45 @@
+export class Ahorcado{
+    constructor(){
+        this.arrayletrasfalladas = [];
+        //Array con las letras de la palabra
+        this.arrayletraspalabra = [];
+        //Array con las letras que el usuario a ido introduciendo y son correctas
+        this.arrayletrasacertadas = [];
+        //String con la palabra deseada;
+        this.palabra = "" ;
+        //Array palabras
+        this.arrayPalabras = ["Ola","Caracol","Confinamiento"] ;
+        //Letras acertadas
+        this.acertadas = 0;
+        //Vidas
+        this.vidas = 7;  
+
+    }
+
+    export getArrayLetrasFalladas(){
+        return this.arrayletrasfalladas;
+    }
+
+    anadirLetraFallada(letra){
+        this.arrayletrasfalladas.push(letra);
+    }
+
+    getArrayLetrasAcertadas(){
+        return this.arrayletrasfalladas;
+    }
+    anadirLetraAcertada(letra){
+        this.arrayletrasacertadas.push(letra);
+    }
+
+    getPalabra(){
+        return this.palabra;
+    }
+
+    setPalabra(){
+        this.palabra=_.random(0, palabras.lenthg());
+    }
+    
+    anadirPalabraAArrayPalabras(palabra){
+        this.arrayPalabras.push(palabra);
+    }
+}
