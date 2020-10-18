a879517bf88943c4ec45e731c9e4c288123dafcc
JAUME PONS ORTÍ <jpons@iesfuentesanluis.org>
2020-10-18 14:43:57 +0200

    Modificado pelota

--- Pelota que sigue el raton/js/index.js
+++ Pelota que sigue el raton/js/index.js
@@ -9,15 +9,43 @@ window.onload = function () {
 function mueveObjeto(unEvento) {
     var evento = window.event || unEvento;
     var X = evento.clientX;
-    var Y = evento.clientY;
+   
+    var XAnterior = -100;
+    var YAnterior = -100;
+    var seHaMovido = false; //Sirve para comprovar si se a movido alguna vez el raton
+    var cont = 2;
+    var ratonDelanteDePelota = false;
+    var xArray = [-100, -100, -100, -100]
+    if (!seHaMovido) {
+        console.log("X:" + X);
 
-    console.log("X:" + X);
-    console.log("pe" + (pelota.offsetLeft ));
-    if (pelota.offsetLeft < X) {
-        pelota.style.left = X - 100 + "px";
-    } else {
-        pelota.style.left = X + 100 + "px";
+        console.log("pe" + (pelota.offsetLeft + 100));
+
+        console.log("CON" + cont);
+        console.log("XA" + xArray[cont-2]);
+        if (xArray[cont-2]>X){
+           ratonDelanteDePelota = false;
+        }else{
+            ratonDelanteDePelota=true;
+        }
+
+        if (pelota.offsetLeft < X) {
+            pelota.style.left = X - 100 + "px";
+        } else {
+            pelota.style.left = X + 100 + "px";
+        }
+
+
+
+        xArray[cont] = X;
+        cont = cont + 1;
+        if (cont >= xArray.length) {
+            cont = 0;
+        }
     }
+    seHaMovido = true;
+}
 
-
+function Media(a) {
+    a
 }
