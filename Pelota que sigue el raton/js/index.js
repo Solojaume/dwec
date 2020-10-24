046062eaebcd1eb424d9a46ccfff39c01299505c
JAUME PONS ORTÍ <jpons@iesfuentesanluis.org>
2020-10-24 11:41:14 +0200

    Algunos cambios al generador de paginas y a la pelota que sigue

--- Pelota que sigue el raton/js/index.js
+++ Pelota que sigue el raton/js/index.js
@@ -13,36 +13,34 @@ function mueveObjeto(unEvento) {
     var XAnterior = -100;
     var YAnterior = -100;
     var seHaMovido = false; //Sirve para comprovar si se a movido alguna vez el raton
-    var cont = 2;
+    var cont = 0;
     var ratonDelanteDePelota = false;
     var xArray = [-100, -100, -100, -100]
     if (!seHaMovido) {
         console.log("X:" + X);
 
-        console.log("pe" + (pelota.offsetLeft + 100));
-
-        console.log("CON" + cont);
-        console.log("XA" + xArray[cont-2]);
-        if (xArray[cont-2]>X){
+        console.log("pe:" + (pelota.offsetLeft));
+        console.log("CON: " + cont);
+        
+        if (pelota.offsetLeft==X){
            ratonDelanteDePelota = false;
         }else{
             ratonDelanteDePelota=true;
         }
 
-        if (pelota.offsetLeft < X) {
+        if (pelota.offsetLeft < X && ratonDelanteDePelota) {
             pelota.style.left = X - 100 + "px";
         } else {
             pelota.style.left = X + 100 + "px";
         }
 
-
-
         xArray[cont] = X;
-        cont = cont + 1;
-        if (cont >= xArray.length) {
+        cont = x=cont=>{return cont+ 1};
+        if (cont <= xArray.length) {
             cont = 0;
         }
     }
+    
     seHaMovido = true;
 }
 
